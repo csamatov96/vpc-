1e1b45dabd7c247387cf0565f1334e4fa0b40fcf
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:23:30 -0600

    changes

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,7 @@
 # main.tf inside <environment> folder
 module "example_vpc" {
   source     = "/root/vpc-/module/vpc"
-  #env        = var.env
+  env        = var.env
   subnet     = "10.0.0.0/24"
   cidr_block = "10.0.0.0/16"
 }
