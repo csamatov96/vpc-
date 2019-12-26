6598dbba065eafb6c6fc7b17c490c2bfb35545a2
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:48:50 -0600

    infra.tfvars added

--- module/variables.tf
+++ module/variables.tf
@@ -9,7 +9,6 @@ variable "env" {
   default = "dev"
 }
 
-/*
 ##sg 
 variable "vpc_security_group_ids" {
     default = "sg-0de690b2dd19af474" #sec group created for created VPC
@@ -19,5 +18,5 @@ variable "vpc_security_group_ids" {
 variable "subnet_id" {
     default = "subnet-0d10f60551aed4327" #created subnet ID
 }
-*/
+
 
---
+++ module/vpc/infra.tfvars
@@ -0,0 +1,14 @@
+## variables.tf of module
+subnet  = "10.0.0.0/24"
+
+cidr_block = "10.0.0.0/16"
+env = "dev"
+
+
+##sg 
+vpc_security_group_ids = "sg-0de690b2dd19af474" #sec group created for created VPC
+
+
+#subnet
+subnet_id = "subnet-0d10f60551aed4327" #created subnet ID
+
