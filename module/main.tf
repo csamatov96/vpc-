e1a5476170d2b7cc1ac292b0014ed11955436074
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:22:59 -0600

    removed

--- module/main.tf
+++ module/main.tf
@@ -6,11 +6,3 @@ module "example_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-# provider.tf
-provider "aws" {
-  version    = "~> 2.22.0"
-  region     = "eu-west-1"
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
-#  alias      = "frankfurt"
-}
