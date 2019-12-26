ffcd99cc551c6f1518bd72213aba23182a1d788b
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:36:34 -0600

    changes

---
+++ module/ec2.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "VM-in-new-vpc" {
+  ami                    = "ami-01f14919ba412de34" #id of desired AMI
+  instance_type          = "t2.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids # list
--- module/vpc/ec2.tf
+++
@@ -1,7 +0,0 @@
-/*resource "aws_instance" "VM-in-new-vpc" {
-  ami                    = "ami-221ea342" #id of desired AMI
-  instance_type          = "m3.medium"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids # list
- 
-}*/
