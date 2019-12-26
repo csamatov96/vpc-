5b6da9e6bc5dbbc26cfe4b182cd0a7b57e2ccee8
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:46:24 -0600

    changes

--- module/ec2.tf
+++ module/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "VM-in-new-vpc" {
   ami                    = "ami-01f14919ba412de34" #id of desired AMI
   instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids # list
+  subnet_id              = "subnet-0d10f60551aed4327"
+  vpc_security_group_ids = "sg-0de690b2dd19af474" # list
 }
--- module/variables.tf
+++ module/variables.tf
@@ -9,6 +9,7 @@ variable "env" {
   default = "dev"
 }
 
+/*
 ##sg 
 variable "vpc_security_group_ids" {
     default = "sg-0de690b2dd19af474" #sec group created for created VPC
@@ -18,5 +19,5 @@ variable "vpc_security_group_ids" {
 variable "subnet_id" {
     default = "subnet-0d10f60551aed4327" #created subnet ID
 }
-
+*/
 
