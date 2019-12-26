7d54243fd4307501a79d0878e597dec53690bba1
csamatov96 <csamatov96@gmail.com>
2019-12-26 15:20:57 -0600

    path edited

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 # main.tf inside <environment> folder
 module "example_vpc" {
-  source     = "../modules/vpc"
+  source     = "/root/vpc-/module/vpc"
   env        = var.env
   subnet     = "10.0.0.0/24"
   cidr_block = "10.0.0.0/16"
